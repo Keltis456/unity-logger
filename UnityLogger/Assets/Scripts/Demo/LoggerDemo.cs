daf6d041252c4560b4317eda8c38a66d4a23f964
Nikita Brik <keltis.kk@gmail.com>
2020-06-17 18:05:26 +0300

    Added logger demo

---
+++ UnityLogger/Assets/Scripts/Demo/LoggerDemo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Logger;
+
+namespace Demo
+{
+    public class LoggerDemo : MonoBehaviour
+    {
+        private readonly ILog log = LogFactory.CreateFor<LoggerDemo>();
+
+        private void Awake()
+        {
+            log.Debug("Awake");
+        }
+
+        private void Start()
+        {
+            log.Debug("Start");
+            log.Warning("Warning");
+            log.Error("Error");
+        }
+    }
+}
